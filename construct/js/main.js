babebd6363802cc7d9b4b59622b8770e30621a49
Alexey Listvennikov <a.listvennikov@smileexpo.ru>
2019-01-23 09:35:32 +0300

    create with type blocks

--- construct/js/main.js
+++ construct/js/main.js
@@ -1,25 +1,32 @@
-const BLOCK_H = {name: null, p: 'ЗАГОЛОВОК | h1'};
-const BLOCK_P = {name: null, p: 'КОНТЕНТ | p'};
-const BLOCK_A = {name: null, p: 'ССЫЛКА | a'};
-const BLOCK_IMG = {name: null, p: 'КАРТИНКА | img'};
-
-// window.onload=function(){
+const BLOCKS = {
+    btn_h : {p: 'ЗАГОЛОВОК | h1'},
+    btn_p : {p: 'КОНТЕНТ | p'},
+    btn_a : {p: 'ССЫЛКА | a'},
+    btn_img : {p: 'КАРТИНКА | img'}
+};
     // const MAIN = document.getElementById('sortable');
 
     // var new_elem = document.getElementById('btn-h');
     // new_elem.addEventListener('click', createBlockH)
 
-    function createBlockH(){
-        console.log(evn);
+    function createBlock(){
+        var evt = event.target;
+        var evt_id = evt.getAttribute('id');
+
+        // <div class='text_block'>
         var div = document.createElement('div');
         div.className = 'text_block';
+        // <p>BLOCKS.btn_h.p</p>
         var p = document.createElement('p');
-        p.innerHTML = BLOCK_H.p;
+        for (var name_block in BLOCKS) {
+            if (name_block == evt_id) {
+                p.innerHTML = BLOCKS[name_block].p;
+            }
+        }
         div.appendChild(p);
+
         var textarea = document.createElement('textarea');
         div.appendChild(textarea);
 
         document.getElementById('sortable').appendChild(div);
     }
-
-// }
